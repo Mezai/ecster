9701c747a2ebba266b944f452a785b5ad9d6c996
root <root@psdevs.com>
2016-09-21 13:30:51 -0400

    fix carrier display

--- views/js/ecstercheckout.js
+++ views/js/ecstercheckout.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
 		} else {
 			$deliveryMethods.insertAfter($orderDetailContents);
 			$account.hide();
-			$carrierArea.hide();
+			$carrierArea.detach();
 			$paymentMethods.hide();
 			$psPayHeader.hide();
 		}
